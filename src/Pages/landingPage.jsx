8c45b6f70592d87e164a25988a98260387b66917
unknown <abdulhilal242@gmail,com>
2023-11-03 23:00:49 +0700

    fix bug card_auth position

--- src/Pages/landingPage.jsx
+++ src/Pages/landingPage.jsx
@@ -8,12 +8,13 @@ const LandingPage = () => {
     <div className="w-full h-screen bg-[url('/Images/BackgroundLanding.svg')] bg-no-repeat bg-cover flex flex-col justify-center">
       <Navbar/>
       <div className="flex flex-col md:flex-row md:justify-between mx-[6%]">
-        <div className="flex flex-col gap-3 mt-[40%] md:mt-[10%] justify-center">
+        <div className="flex flex-col gap-3 mt-[60%] md:mt-[10%] justify-center">
           <h1 className="md:w-[75%] text-tBlue font-bold text-[15px] md:text-[28px] text-left">Academic Monotoring System memantau perkembangan akademik anak </h1>
           <p className="md:w-[70%] text-[12px] md:text-[15px] font-medium">Dapatkan akses real-time untuk memantau nilai, kehadiran dan tugas anak Anda secara terperinci.</p>
           <Button classname="w-[150px] lg:w-[210px] text-white bg-tBlue text-[12px] mx-auto mt-4">Panduan</Button>
         </div>
-        <CardAuth/>        
+        <CardAuth/>
+        <div className="mb-[50px]"></div>        
       </div>
 
     </div>
