a8a229fb8c128f6a1106a11a1bbb3f9dd369ad8a
unknown <abdulhilal242@gmail,com>
2023-11-12 23:44:16 +0700

    solve bug image deployment 15

--- src/Pages/dashboardPage.jsx
+++ src/Pages/dashboardPage.jsx
@@ -12,9 +12,9 @@ const DashboardPage = () => {
         <p className="w-[60%] md:w-[40%] text-[12px] md:text-[15px] 2xl:text-[18px] font-medium animate-fade animate-delay-300">Dapatkan akses real-time untuk memantau nilai, kehadiran dan tugas anak Anda secara terperinci.</p>
       </div>
       <div className="w-full flex flex-col md:flex-row justify-between items-center px-36 mt-20 animate-fade-right">
-        <CardMain title={'Kemahasiswaan'} icons={`/public/images/kemahasiswaan.png` || `images/kemahasiswaan.png`} content={["Hubungi Dosen Pembimbing Akademik","Kartu Hasil Studi dan Evaluasi"]}/>
-        <CardMain title={'Akademik'} icons={`/public/images/akademik.png` || `images/akademik.png`} content={["Statistik IPK","Jadwal Kuliah","KHS Evaluasi","Kehadiran"]}/>
-        <CardMain title={'Informasi'} icons={`/public/images/informasi.png` || `images/informasi.png`} content={["Akademik","Kalender Akademik","Biaya","Terkini"]}/>
+        <CardMain title={'Kemahasiswaan'} icons={`/public/images/kemahasiswaan.png` || `/images/kemahasiswaan.png`} content={["Hubungi Dosen Pembimbing Akademik","Kartu Hasil Studi dan Evaluasi"]}/>
+        <CardMain title={'Akademik'} icons={`/public/images/akademik.png` || `/images/akademik.png`} content={["Statistik IPK","Jadwal Kuliah","KHS Evaluasi","Kehadiran"]}/>
+        <CardMain title={'Informasi'} icons={`/public/images/informasi.png` || `/images/informasi.png`} content={["Akademik","Kalender Akademik","Biaya","Terkini"]}/>
       </div>
       <div className="mt-5"></div>
     </div>
