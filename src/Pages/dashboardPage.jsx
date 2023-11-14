3127ad2ca1424768b3b76922f9f57b193002c549
unknown <abdulhilal242@gmail,com>
2023-11-15 00:42:40 +0700

    responsive dashboard page complete 2

--- src/Pages/dashboardPage.jsx
+++ src/Pages/dashboardPage.jsx
@@ -9,7 +9,7 @@ const DashboardPage = () => {
       <Navbar types={"dashboard-navbar"} />  
       <div className="w-full flex flex-col mx-auto mt-[40%] md:mt-[12%] lg:mt-[9%] justify-center items-center">
         <h1 className="text-[15px] md:text-[30px] lg:text-[38px] 2xl:text-[42px] font-bold text-tBlue animate-fade animate-delay-200">Academic Monitoring System</h1>
-        <p className=" w-[55%] md:w-[50%] lg:w-[40%] text-[12px] md:text-[15px] 2xl:text-[18px] font-medium animate-fade animate-delay-300">Dapatkan akses real-time untuk memantau nilai, kehadiran dan tugas anak Anda secara terperinci.</p>
+        <p className=" w-[55%] md:w-[50%] lg:w-[40%] 2xl:w-[35%] text-[12px] md:text-[15px] 2xl:text-[18px] font-medium animate-fade animate-delay-300">Dapatkan akses real-time untuk memantau nilai, kehadiran dan tugas anak Anda secara terperinci.</p>
       </div>
       <div className="w-full flex flex-col lg:flex-row justify-center gap-3 lg:gap-20 2xl:gap-24 items-center px-36 mt-20 mb-10 animate-fade-right">
         <CardMain title={'Kemahasiswaan'} imageUrl="https://amsys.vercel.app/images/kemahasiswaan.png" navigate={""} content={["Hubungi Dosen Pembimbing Akademik","Kartu Hasil Studi dan Evaluasi"]}/>
