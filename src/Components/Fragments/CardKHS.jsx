fc6b2e453488683a210d6909317f93559e7d7d55
unknown <abdulhilal242@gmail,com>
2023-11-26 15:15:52 +0700

    responsive khs dan evaluasi complete

--- src/Components/Fragments/CardKHS.jsx
+++ src/Components/Fragments/CardKHS.jsx
@@ -1,11 +1,11 @@
-const CardKHS = () => {
+const CardKHS = ({semester,predikat,ipk}) => {
   return (
     <div className="w-full max-w-xs flex flex-col items-center gap-6 rounded-lg shadow-lg p-7">
       <div className="w-full flex flex-row justify-between">
-        <h1 className="font-semibold text-lg">Semester 1</h1>
-        <span className="text-tBlue text-sm italic">Sangat Baik</span>
+        <h1 className="font-semibold text-lg">Semester {semester}</h1>
+        <span className="text-tBlue text-sm italic">{predikat}</span>
       </div>
-      <span className="font-semibold text-4xl text-tBlue">4.00</span>
+      <span className="font-semibold text-4xl text-tBlue">{ipk}</span>
       <div className="flex flex-col items-center gap-1">
         <button className="w-full max-w-[200px] bg-tBlue text-sm text-white rounded-lg p-1">Tampilkan</button>
         <p className="font-extralight text-xs text-[#9C9191]">klik <strong>Tampilkan </strong>untuk melihat rincian nilai</p>
--- src/Components/Layouts/Carrousels.jsx
+++ src/Components/Layouts/Carrousels.jsx
@@ -1,30 +1,54 @@
 import CardKHS from "../Fragments/CardKHS"
 
+const dataIPK = [
+  {
+    semester:1,
+    predikat:'Sangat Baik',
+    ipk:3.89
+  },
+  {
+    semester:2,
+    predikat:'Sangat Baik',
+    ipk:3.90
+  },
+  {
+    semester:3,
+    predikat:'Sangat Baik',
+    ipk:3.91
+  },
+  {
+    semester:4,
+    predikat:'Sangat Baik',
+    ipk:3.92
+  },
+  {
+    semester:5,
+    predikat:'Sangat Baik',
+    ipk:3.93
+  },
+  {
+    semester:6,
+    predikat:'Sangat Baik',
+    ipk:3.94
+  },
+  {
+    semester:7,
+    predikat:'Sangat Baik',
+    ipk:3.97
+  },
+
+]
 
 const Carrousels = () => {
   return (
     <div className="w-full max-w-[930px] carousel rounded-box gap-4 p-6">
-      <div className="carousel-item">
-        <CardKHS/>
-      </div> 
-      <div className="carousel-item">
-        <CardKHS/>
-      </div> 
-      <div className="carousel-item">
-        <CardKHS/>
-      </div> 
-      <div className="carousel-item">
-        <CardKHS/>
-      </div> 
-      <div className="carousel-item">
-        <CardKHS/>
-      </div> 
-      <div className="carousel-item">
-        <CardKHS/>
-      </div> 
-      <div className="carousel-item">
-        <CardKHS/>
-      </div>
+      {
+        dataIPK.map((item, index) => (
+          <div className="carousel-item" key={index}>
+            <CardKHS semester={item.semester} predikat={item.predikat} ipk={item.ipk}/>
+          </div>           
+        ))
+      }      
     </div>
   )
 }
